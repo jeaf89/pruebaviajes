bb4fea8f21b63f067ce2638440246d38c30a2c83
7acuerda <rodrigo@7acuerda.cl>
2022-04-23 16:33:30 -0400

    funcion java para cada card

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function btnChanger() {
+
+    let bg = document.getElementById("containerbtn").style.background = "black";
+
+}
+
+function btnChanger2() {
+
+    let bg = document.getElementById("containerbtn2").style.background = "black";
+
+}
+
+function btnChanger3() {
+
+    let bg = document.getElementById("containerbtn3").style.background = "black";
+
+}
+
+function btnChanger4() {
+
+    let bg = document.getElementById("containerbtn4").style.background = "black";
+
+}
